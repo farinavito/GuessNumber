5ce632801f49ed42f06931dff2f7f996413711a9
Vito Farina <farinavito@gmail.com>
2019-11-18 22:09:23 +0100

    Initial commit

---
+++ GuessNumber.py
@@ -0,0 +1,31 @@
+import random
+
+with open("score.txt", "r") as score_file:
+    old_scores_raw = score_file.read()
+old_scores_raw = old_scores_raw.split("\n")
+
+old_scores = []
+for i in old_scores_raw[:-1]:
+    old_scores.append(int(i))
+
+min_score = min(old_scores)
+print(f"Best result so far {min_score}")
+
+
+secret_num = random.randint(1, 100)
+attempts = 0
+
+while True:
+    guess = int(input("Ugibaj stevilo med 1 in 100: "))
+    attempts += 1
+
+    if guess < secret_num:
+        print("Ugibaj visje")
+    elif guess > secret_num:
+        print("Ugibaj nizje")
+    else:
+        print(f"Bravo, uganil si v {attempts} poizkusih.")
+        break
+
+with open("score.txt", "a") as score_file:
+    score_file.write(f"{attempts}\n")
